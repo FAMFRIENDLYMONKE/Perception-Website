1bc1fc4b7318405669ed03543cea54eb123f8ead
shanks-rahul <rahulagarwalla1234@gmail.com>
2025-01-22 14:59:41 +0530

    added event lists with search bar

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,7 +7,46 @@ import PVPGaming from './PVP Gaming.jpg';
 import RescueRangers from './Rescue Rangers.jpg';
 import SensorSyncSymposium from './SensorSync Symposium.jpg';
 import WearableTech from './Wearable Tech.jpg';
+import AerospaceWaterpoweredbottleRocket from './AeropsaceWaterpoweredbottleRocket_4-2.jpg';
+import Aerospace76105RCAirplane from './Aerospace76105-rc-airplane.webp';
+import AerospaceSlingshotTech from './Aerospaceslingshot tech.jpg';
+import AerospaceTech from './Aerospacetech.jpg';
+import AerospaceTinyWhoop from './AerospacetinyWhoop.webp';
+import AerospaceWingman from './Aerospacewingman.jpg';
 
+// BiozoEvents (example placeholder as no images are listed in this folder)
+
+// CiviconEvents
+import CiviconBridgeMaking from './CiviconBridgeMaking.jpg';
+import CiviconCanvaDesign from './CiviconCanvaDesign2.jpg';
+import CiviconEscapeRoom from './CiviconescapeRoom.jpg';
+import CiviconWheresTheMystery from './CiviconWheresTheMystery.jpg';
+
+//EnergyEvents
+import EnergyCircuitCraft from './EnergyCircuitCraft.jpg';
+import EnergyElectrovia from './EnergyElectrovia.jpg';
+import EnergyGameMania from './EnergyGameMania.jpg';
+import EnergyLineFollower from './EnergyLineFollower.jpg';
+
+// SAEvents
+import SAEFrameForge from  './SAEFrameForge.jpg';
+import SAELeadTheBeast from './SAELeadTheBeast.jpg';
+import SpectrumCirKit from './SpectrumCirKit.jpg';
+import SpectrumDesignAthon from './SpectrumDesignAthon.jpg';
+import SpectrumEditExpo from './SpectrumEditExpo.jpg';
+import SpectrumGameCraft from './SpectrumGameCraft.jpg';
+import SpectrumRoboSoccer from './SpectrumRoboSoccer.jpg';
+import SpectrumSpecMoji from './SpectrumSpecMoji.jpg';
+import SpectrumTechMeme from './SpectrumTechMeme.jpg';
+import SpectrumUncannyCodeQuest from './SpectrumUncannyCodeQuest.jpg';
+
+// ZairzaEvents
+import ZairzaBountyHunter from './ZairzaBountyHunter.jpg';
+import ZairzaHumanLudo from './ZairzaHumanLudo.jpg';
+import ZairzaPVPGaming from './ZairzaPVPGaming.jpg';
+import ZairzaRescueRangers from './ZairzaRescuerangers.webp';
+import ZairzaSensorSync from './ZairzaSensorSync.jpg';
+import ZairzaWearableTech from './ZairzaWearableTech.jpg';
 
 // Export all images
 export {
@@ -20,5 +59,33 @@ export {
   RescueRangers,
   SensorSyncSymposium,
   WearableTech,
-
+  Aerospace76105RCAirplane,
+  AerospaceSlingshotTech,
+  AerospaceTech,
+  AerospaceTinyWhoop,
+  AerospaceWaterpoweredbottleRocket,
+  AerospaceWingman,
+  CiviconBridgeMaking,
+  CiviconCanvaDesign,
+  CiviconEscapeRoom,
+  CiviconWheresTheMystery,
+  EnergyCircuitCraft,
+  EnergyElectrovia,
+  EnergyGameMania,
+  EnergyLineFollower,
+  SAEFrameForge,
+  SAELeadTheBeast,
+  SpectrumCirKit,
+  SpectrumDesignAthon,
+  SpectrumEditExpo,
+  SpectrumGameCraft,
+  SpectrumRoboSoccer,
+  SpectrumSpecMoji,
+  SpectrumTechMeme,
+  SpectrumUncannyCodeQuest,
+  ZairzaBountyHunter,
+  ZairzaHumanLudo,
+  ZairzaPVPGaming,
+  ZairzaRescueRangers,ZairzaSensorSync,
+  ZairzaWearableTech,
 };
