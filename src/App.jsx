52f44d05bff963b9fe4bc82bf3601fadc2f6077c
Suveshh <suvesh2003@gmail.com>
2025-01-24 11:03:06 +0530

    updated event

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Events from "./Pages/Events";
 import Merch from "./Pages/Merch";
-import FlagshipEvents from "./Pages/FlagshipEvents"
-
+import FlagshipEvents from "./Pages/FlagshipEvents";
+import Card from "./components/Card";
 
 function App() {
   return (
     <Routes>
-    <Route path="/" element={<Layout name={name} />}>
-      <Route path="" element={<Home />} />
-      <Route path="/events" element={<Events/>} />
-      <Route path="/merch" element={<Merch/>} />
-      <Route path="/events" element={<FlagshipEvents/>} />
-    </Route>
-  </Routes>
+      <Route path="/" element={<Layout name={name} />}>
+        <Route path="" element={<Home />} />
+        <Route path="/events" element={<Card />} />
+        <Route path="/merch" element={<Merch />} />
+        <Route path="/events" element={<FlagshipEvents />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,51 @@
 import React from "react";
+import { flagshipevents } from "../events.js";
 
 const Card = () => {
   return (
-    <div className="mt-24 flex flex-col md:flex-row items-center bg-none p-8 rounded-xl shadow-lg max-w-[80%] lg:max-w-[1000px] mx-auto">
-      <div className="flex mx-auto relative max-h-[530px]">
-        <img
-          src="https://images.pexels.com/photos/9943963/pexels-photo-9943963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Skincare Model"
-          className="rounded-lg max-w-[100%]"
-        />
-        <div className="absolute bottom-0 rounded-lg w-fit leading-tight">
-          <h1 className="text-5xl font-bold text-white relative top-4">
-            <p className="bg-glass px-6 py-1 rounded-tr-2xl inline-block">BASED</p> <br />
-            <p className="bg-glass px-6 py-1
-            rounded-tr-2xl inline-block">PEPTIDE</p> <br />
-            <p className="bg-glass px-6 py-1 pb-2 rounded-r-2xl inline-block">SKINCARE</p>
-          </h1>
-          <p className="mt-4 text-xs text-gray-300 w-[200px] bg-glass py-2 px-6">
-            Discover The World Of Step-By-Step Care With Skincare Kit By Estee
-            Lauder.
-          </p>
-          <div className="bg-glass px-6 py-4 rounded-tr-2xl rounded-bl-lg">
-            <button className="px-16 py-2 bg-black text-white rounded-full hover:bg-gray-800">
-              BUY KIT $119.00
-            </button>
+    <div className="flex flex-col items-center">
+      {flagshipevents.map((event, index) => (
+        <div
+          key={index}
+          className="mt-24 flex flex-col md:flex-row items-center bg-none p-8 rounded-xl shadow-lg max-w-[80%] lg:max-w-[1000px] mx-auto"
+        >
+          <div className="flex mx-auto relative max-h-[530px]">
+            <img
+              src={event.image}
+              alt="Skincare Model"
+              className="rounded-lg max-w-[100%]"
+            />
+            <div className="absolute bottom-0 rounded-lg w-fit leading-tight">
+              <h1 className="text-5xl font-bold text-white relative top-4">
+                <p className="bg-glass px-6 py-1 rounded-tr-2xl rounded-br-lg inline-block">
+                  {event.eventName}
+                </p>
+              </h1>
+              <p className="mt-4 text-xs text-gray-300 max-w-[300px] bg-glass py-2 px-6">
+                {event.description}
+              </p>
+              <div className="bg-glass px-6 py-4 rounded-tr-2xl rounded-bl-lg">
+                <button className="px-28 py-3 bg-black text-white rounded-full hover:bg-gray-800">
+                  Register
+                </button>
+              </div>
+            </div>
+
+            <div className="absolute top-8 left-8 bg-glass p-2 rounded shadow-lg">
+              <p className="text-lg font-medium text-gray-300">
+                {event.clubName}
+              </p>
+            </div>
+            <div className="absolute bottom-8 right-8 bg-glass p-4 rounded shadow-lg">
+              <p className="text-sm font-bold text-gray-300">
+                {event.startTime}
+              </p>
+              <p className="text-sm font-bold text-gray-300">{event.endTime}</p>
+              <p className="text-lg font-bold text-green-600">{event.date}</p>
+            </div>
           </div>
         </div>
-
-        <div className="absolute top-8 left-8 bg-glass p-2 rounded shadow-lg">
-          <p className="text-sm font-medium text-gray-300">REVITALIZING MASK</p>
-          <p className="text-xs text-gray-400">50ml/1.69oz</p>
-        </div>
-        <div className="absolute bottom-8 right-8 bg-glass p-4 rounded shadow-lg">
-          <p className="text-sm font-medium text-gray-300">DAILY CREAM</p>
-          <p className="text-xs text-gray-400">75ml/2.5oz</p>
-          <p className="text-sm font-bold text-gray-300 line-through">$35.65</p>
-          <p className="text-lg font-bold text-green-600">$29.55</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
