c51312b1579a6fe6865d00067187aceab71dc290
FAMFRIENDLYMONKE <84069775+FAMFRIENDLYMONKE@users.noreply.github.com>
2025-01-25 10:51:30 +0530

    edit pic

--- src/components/Techsec.jsx
+++ src/components/Techsec.jsx
@@ -1,7 +1,7 @@
 import React,{useEffect} from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import pic from "../assets/anshuman.jpg";
+import pic from "../assets/EnergyGameMania.jpg";
 
 function Techsec() {
 
